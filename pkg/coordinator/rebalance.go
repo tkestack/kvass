471a6882a5d532c2f6767eea141438cc4a17db43
RayHuangCN <raylhuang@tencent.com>
2021-05-25 14:40:58 +0800

    clea: remove some log

--- pkg/coordinator/rebalance.go
+++ pkg/coordinator/rebalance.go
@@ -213,7 +213,6 @@ func (c *Coordinator) alleviateShards(changeAbleShards []*shardInfo) (needSpace 
 	for _, s := range changeAbleShards {
 		for _, t := range threshold {
 			if s.runtime.HeadSeries >= seriesWithRate(c.maxSeries, t.maxSeriesRate) {
-				c.log.Infof("%s series is %d, over rate %f", s.shard.ID, s.runtime.HeadSeries, t.maxSeriesRate)
 				needSpace += c.alleviateShard(s, changeAbleShards, seriesWithRate(c.maxSeries, t.expectSeriesRate))
 				break
 			}
